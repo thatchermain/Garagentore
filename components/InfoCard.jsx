eaee487d5d0d453cb185ca3bbbd7a4fbdc24e581
thatchermain <thatchermain@gmail.com>
2022-01-29 10:22:30 +0100

    INFO ICON SIZE CHANGE

--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -12,7 +12,7 @@ const InfoCard = (props) => {
         className={`${styles.card} d-flex flex-column justify-content-around align-items-center p-3 `}
       >
         <h3 className={styles.title}>{props.title}</h3>
-        <FontAwesomeIcon className={styles.icon} icon={props.icon} width={50} />
+        <FontAwesomeIcon className={styles.icon} icon={props.icon} />
         <p className={styles.text}>{props.text}</p>
         <button className={styles.btn}>BITTE ANKLICKEN</button>
       </div>
